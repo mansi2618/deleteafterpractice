d7b8e65fa757507f750340afa20a3b6b3b40f1d8
mansi <mahabale1998@gmail.com>
2026-07-31 10:07:33 +0530

    saa

---
+++ tfb44/ec2datablock.tf
@@ -0,0 +1,67 @@
+provider "aws" {
+  region = "ap-south-1"
+  profile = "configs"
+} 
+
+
+
+resource "aws_instance"  "webserver" {
+    ami = var.amiid
+    instance_type = var.insttype
+    vpc_security_group_ids =  [ var.sg , aws_security_group.webserver_sg.id , data.aws_security_group.sgdata.id ]
+    key_name = var.kp
+    tags = {
+        purpose = "webserver"
+    }
+    #count = var.instno
+    disable_api_termination = var.apiterm
+    user_data = <<-EOF
+                 #!/bin/bash
+                 sudo yum install nginx -y
+                 sudo systemctl start nginx
+                EOF
+
+}
+
+
+resource "aws_security_group" "webserver_sg" {
+    name = "tf-sg"
+    ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "TCP"
+      cidr_blocks  = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks  = ["0.0.0.0/0"]
+    }
+
+
+    egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks  = ["0.0.0.0/0"]
+
+    }
+
+}
+
+
+
+data "aws_security_group" "sgdata" {
+    name = "launch-wizard-5"
+}
+
+
+output "instaneip" {
+   value = aws_instance.webserver.public_ip
+}
+
+output "pubdns" {
+  value = aws_instance.webserver.public_dns
+}
---
+++ tfb44/vardatablock.tf
@@ -0,0 +1,23 @@
+variable "amiid" {
+    default = "ami-00d2dbb426772b03a"
+}
+
+variable "insttype" {
+    default = "t3.micro"
+}
+
+variable "sg" {
+   default = "sg-0900552f2cde33080"
+}
+
+variable "kp" {
+    default = "key"
+
+}
+
+
+#variable "instno" {default = 2}
+
+variable "apiterm" {
+    default = false
+}
