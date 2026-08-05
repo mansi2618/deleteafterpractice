f671d4a76346753358c2452a4764e08e16f679c4
mansi <mahabale1998@gmail.com>
2026-08-05 09:13:51 +0530

    committ

---
+++ tfb44/var_lb.tf
@@ -0,0 +1,23 @@
+variable "amiid" {
+    default = "ami-035827357e3c7e810"
+}
+
+variable "insttype" {
+    default = "t3.micro"
+}
+
+variable "sg" {
+   default = "sg-0900552f2cde33080"
+}
+
+variable "kp" {
+    default = "key"
+
+}
+
+
+#variable "instno" {default = 2}
+
+variable "apiterm" {
+    default = false
+}
